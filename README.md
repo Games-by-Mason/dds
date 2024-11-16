8ff599cebbe27b117b6337d1db58148966947dc3
Mason Remaley <mason@anthropicstudios.com>
2024-11-16 02:53:12 -0800

    Improves alpha options

--- README.md
+++ README.md
@@ -5,9 +5,9 @@ A **work in progress** DDS utility for Zig.
 # Plan
 * [x] Provide DDS header structures for reading DDS files in Zig
 * [x] Support converting PNGs to uncompressed DDS files with and without alpha
-* [ ] Check the PNG header, and fail if requested alpha but none is present
 * [ ] Support converting PNGs to BC7 compressed DDS files using [bc7enc_rdo_zig](https://github.com/Games-by-Mason/bc7enc_rdo_zig)
-	* Look into whether other modes are needed for alpha, no alpha, normal maps, etc
+* [ ] Look into whether other modes are needed for alpha, no alpha, normal maps, etc
+* [ ] Look into srgb/alpha premul conversions
 * [ ] Support applying lossless compression to the resulting DDS
 * [ ] Support automatic mipmap generation
 * [ ] Look into why lodepng fails UBSAN
--- src/main.zig
+++ src/main.zig
@@ -15,14 +15,19 @@ const c = @cImport({
 
 const max_file_len = 4294967296;
 
+const Format = enum {
+    auto,
+    rgb,
+    rgba,
+};
+
 const command: Command = .{
     .name = "dds",
     .description = "Converts PNG to DDS.",
     .named_args = &.{
-        NamedArg.init(bool, .{
-            .long = "alpha",
-            .short = 'a',
-            .default = .{ .value = false },
+        NamedArg.init(Format, .{
+            .long = "format",
+            .default = .{ .value = .auto },
         }),
     },
     .positional_args = &.{
@@ -36,6 +41,7 @@ const command: Command = .{
 };
 
 pub fn main() !void {
+    // Setup
     defer std.process.cleanExit();
 
     var gpa = std.heap.GeneralPurposeAllocator(.{ .thread_safe = false }){};
@@ -49,6 +55,7 @@ pub fn main() !void {
 
     const cwd = std.fs.cwd();
 
+    // Load the PNG
     var input = cwd.openFile(args.INPUT, .{}) catch |err| {
         log.err("{s}: {s}", .{ args.INPUT, @errorName(err) });
         std.process.exit(1);
@@ -61,6 +68,33 @@ pub fn main() !void {
     };
     defer allocator.free(input_bytes);
 
+    // Determine the input and output formats
+    const input_alpha = b: {
+        var width: c_uint = 0;
+        var height: c_uint = 0;
+        var state: c.LodePNGState = .{};
+        assert(c.lodepng_inspect(
+            &width,
+            &height,
+            &state,
+            input_bytes.ptr,
+            input_bytes.len,
+        ) == 0);
+        break :b c.lodepng_is_alpha_type(&state.info_png.color) != 0;
+    };
+    const output_alpha = switch (args.format) {
+        .auto => input_alpha,
+        .rgb => false,
+        .rgba => b: {
+            if (!input_alpha) {
+                log.err("{s}: requested alpha, but none present", .{args.INPUT});
+                std.process.exit(1);
+            }
+            break :b true;
+        },
+    };
+
+    // Decode the PNG
     var uncompressed_ptr: [*c]u8 = undefined;
     var width: c_uint = 0;
     var height: c_uint = 0;
@@ -69,15 +103,15 @@ pub fn main() !void {
         &width,
         &height,
         input_bytes.ptr,
-        @intCast(input_bytes.len),
-        if (args.alpha) c.LCT_RGBA else c.LCT_RGB,
+        input_bytes.len,
+        if (output_alpha) c.LCT_RGBA else c.LCT_RGB,
         8,
     ) != 0) {
         log.err("{s}: lodepng failed", .{args.INPUT});
         std.process.exit(1);
     }
     defer c.free(uncompressed_ptr);
-    const uncompressed = if (args.alpha) uncompressed_ptr[0 .. width * height * 4] else uncompressed_ptr[0 .. width * height * 3];
+    const uncompressed = if (output_alpha) uncompressed_ptr[0 .. width * height * 4] else uncompressed_ptr[0 .. width * height * 3];
 
     var output = cwd.createFile(args.OUTPUT, .{}) catch |err| {
         log.err("{s}: {s}", .{ args.OUTPUT, @errorName(err) });
@@ -88,37 +122,40 @@ pub fn main() !void {
         output.close();
     }
 
-    // Write the four character code
-    output.writeAll("DDS ") catch |err| {
-        log.err("{s}: {s}", .{ args.OUTPUT, @errorName(err) });
-        std.process.exit(1);
-    };
+    // Write the DDS file
+    {
+        // Write the four character code
+        output.writeAll("DDS ") catch |err| {
+            log.err("{s}: {s}", .{ args.OUTPUT, @errorName(err) });
+            std.process.exit(1);
+        };
 
-    // Write the header
-    comptime assert(builtin.cpu.arch.endian() == .little);
-    const header: Dds.Header = .{
-        .height = height,
-        .width = width,
-        .ddspf = .{
-            .flags = .{
-                .alphapixels = args.alpha,
-                .rgb = true,
+        // Write the header
+        comptime assert(builtin.cpu.arch.endian() == .little);
+        const header: Dds.Header = .{
+            .height = height,
+            .width = width,
+            .ddspf = .{
+                .flags = .{
+                    .alphapixels = output_alpha,
+                    .rgb = true,
+                },
+                .rgb_bit_count = if (output_alpha) 32 else 24,
+                .r_bit_mask = 0x00ff0000,
+                .g_bit_mask = 0x0000ff00,
+                .b_bit_mask = 0x000000ff,
+                .a_bit_mask = 0xff000000,
             },
-            .rgb_bit_count = if (args.alpha) 32 else 24,
-            .r_bit_mask = 0x00ff0000,
-            .g_bit_mask = 0x0000ff00,
-            .b_bit_mask = 0x000000ff,
-            .a_bit_mask = 0xff000000,
-        },
-    };
-    output.writeAll(std.mem.asBytes(&header)) catch |err| {
-        log.err("{s}: {s}", .{ args.OUTPUT, @errorName(err) });
-        std.process.exit(1);
-    };
+        };
+        output.writeAll(std.mem.asBytes(&header)) catch |err| {
+            log.err("{s}: {s}", .{ args.OUTPUT, @errorName(err) });
+            std.process.exit(1);
+        };
 
-    // Write the pixels
-    output.writeAll(uncompressed) catch |err| {
-        log.err("{s}: {s}", .{ args.OUTPUT, @errorName(err) });
-        std.process.exit(1);
-    };
+        // Write the pixels
+        output.writeAll(uncompressed) catch |err| {
+            log.err("{s}: {s}", .{ args.OUTPUT, @errorName(err) });
+            std.process.exit(1);
+        };
+    }
 }
